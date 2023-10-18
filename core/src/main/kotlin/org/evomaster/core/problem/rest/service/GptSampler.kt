6f8d230faaf15ce6679422510b1dff9150684911
LarsSven <l.s.andringa@rug.nl>
2023-10-18 13:50:56 +0200

    Added more code for the GPT sampler

--- core/src/main/kotlin/org/evomaster/core/problem/rest/service/GptSampler.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/rest/service/GptSampler.kt
@@ -1,13 +1,49 @@
 package org.evomaster.core.problem.rest.service
 
+import org.evomaster.core.problem.enterprise.SampleType
+import org.evomaster.core.problem.httpws.auth.HttpWsAuthenticationInfo
+import org.evomaster.core.problem.httpws.auth.NoAuth
+import org.evomaster.core.problem.rest.RestCallAction
 import org.evomaster.core.problem.rest.RestIndividual
 
 class GptSampler : AbstractRestSampler() {
     override fun customizeAdHocInitialIndividuals() {
-        TODO("Not yet implemented")
+        adHocInitialIndividuals.clear()
+
+        sampleEachEndpoint(NoAuth())
+
+        authentications.forEach { auth ->
+            sampleEachEndpoint(auth)
+        }
+    }
+
+    private fun sampleEachEndpoint(auth: HttpWsAuthenticationInfo) {
+        actionCluster.asSequence()
+            .filter { a -> a.value is RestCallAction }
+            .forEach { a ->
+                val copy = a.value.copy() as RestCallAction
+                copy.auth = auth
+                copy.doInitialize(randomness)
+                val ind = createIndividual(SampleType.SMART, mutableListOf(copy))
+                ind.doGlobalInitialize(searchGlobalState)
+                adHocInitialIndividuals.add(ind)
+            }
     }
 
     override fun sampleAtRandom(): RestIndividual {
-        TODO("Not yet implemented")
+        val actions = mutableListOf<RestCallAction>()
+        val n = randomness.nextInt(1, getMaxTestSizeDuringSampler())
+
+        (0 until n).forEach {
+            actions.add(generateRestCall())
+        }
+
+        val ind = RestIndividual(actions, SampleType.RANDOM, mutableListOf(), this, time.evaluatedIndividuals)
+        ind.doGlobalInitialize(searchGlobalState)
+        return ind
+    }
+
+    fun generateRestCall(): RestCallAction {
+
     }
 }
